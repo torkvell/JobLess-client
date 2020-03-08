8ff200b2715e44dcc6962cbea25584f69ba5ba85
torkvell <tor.kvell@outlook.com>
2020-03-08 18:23:22 +0100

    added apollo provider to app

--- App.tsx
+++ App.tsx
@@ -2,18 +2,24 @@ import React from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
 import App from './src';
 import { theme } from './src/core/theme';
-// Imports: Redux Persist Persister
 import { store, persistor } from './src/store';
-// Imports: Dependencies
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+const client = new ApolloClient({
+  uri: 'https://localhose:4000/graphql',
+});
 
 const Main = () => (
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <PaperProvider theme={theme}>
-        <App />
-      </PaperProvider>
+      <ApolloProvider client={client}>
+        <PaperProvider theme={theme}>
+          <App />
+        </PaperProvider>
+      </ApolloProvider>
     </PersistGate>
   </Provider>
 );
