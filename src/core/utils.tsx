fd221a804971417c630431a06a587f599dfcf19d
torkvell <tor.kvell@outlook.com>
2020-03-12 13:38:45 +0100

    Inputs to add job specs ready to be sent to apollo mutation hook

--- src/core/utils.tsx
+++ src/core/utils.tsx
@@ -9,12 +9,42 @@ export const emailValidator = (email: string) => {
 
 export const passwordValidator = (password: string) => {
   if (!password || password.length <= 0) return 'Password cannot be empty.';
-
   return '';
 };
 
 export const nameValidator = (name: string) => {
   if (!name || name.length <= 0) return 'Name cannot be empty.';
+  return '';
+};
 
+export const titleValidator = (title: string) => {
+  if (!title || title.length <= 0) return 'Title cannot be empty.';
+  return '';
+};
+
+export const descriptionValidator = (description: string) => {
+  if (!description || description.length <= 25)
+    return 'Description cannot be less than 25 characters.';
+  return '';
+};
+
+export const priceValidator = (price: String) => {
+  if (!price || price.length <= 0) return 'Price cannot be empty';
+  return '';
+};
+
+export const cityValidator = (city: String) => {
+  if (!city || city.length <= 0) return 'City cannot be empty';
+  return '';
+};
+
+export const postalCodeValidator = (postalCode: String) => {
+  if (!postalCode || postalCode.length <= 0)
+    return 'PostalCode cannot be empty';
+  return '';
+};
+
+export const addressValidator = (address: String) => {
+  if (!address || address.length <= 0) return 'Address cannot be empty';
   return '';
 };
--- src/screens/user/MyJobScreen.tsx
+++ src/screens/user/MyJobScreen.tsx
@@ -1,10 +1,34 @@
 import React, { memo, useState } from 'react';
 import Background from '../../components/Background';
+import Container from '../../components/Container';
 import Button from '../../components/Button';
 import { Navigation } from '../../types';
 import { connect } from 'react-redux';
-import { Modal, View, Text, StyleSheet } from 'react-native';
+import {
+  Modal,
+  View,
+  Text,
+  StyleSheet,
+  Alert,
+  Linking,
+  Image,
+  TouchableHighlight,
+  ScrollView,
+} from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
+import TextInput from '../../components/TextInput';
+import {
+  titleValidator,
+  descriptionValidator,
+  priceValidator,
+  cityValidator,
+  postalCodeValidator,
+  addressValidator,
+} from '../../core/utils';
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
+import Constants from 'expo-constants';
+import { theme } from '../../core/theme';
 
 type Props = {
   navigation: Navigation;
@@ -12,24 +36,197 @@ type Props = {
 
 const MyJobScreen = ({ navigation }: Props) => {
   const [modalOpen, setModalOpen] = useState(false);
+  const [title, setTitle] = useState({ value: '', error: '' });
+  const [description, setDescription] = useState({ value: '', error: '' });
+  const [price, setPrice] = useState({ value: null, error: '' });
+  const [images, setImage] = useState({ uri: [], error: '' });
+  const [city, setCity] = useState({ value: null, error: '' });
+  const [postalCode, setPostalCode] = useState({ value: null, error: '' });
+  const [address, setAddress] = useState({ value: null, error: '' });
+  const [hasCameraPermission, setCameraPermission] = useState({
+    value: null,
+    error: '',
+  });
+
+  const validateInput = () => {
+    const titleError = titleValidator(title.value);
+    const descriptionError = descriptionValidator(description.value);
+    const priceError = priceValidator(price.value);
+    const cityError = cityValidator(city.value);
+    const postalCodeError = postalCodeValidator(postalCode.value);
+    const addressError = addressValidator(address.value);
+    if (titleError) {
+      setTitle({ ...title, error: titleError });
+    }
+    if (descriptionError) {
+      setDescription({ ...description, error: descriptionError });
+    }
+    if (priceError) {
+      setPrice({ ...price, error: priceError });
+    }
+    if (cityError) {
+      setCity({ ...city, error: cityError });
+    }
+    if (postalCodeError) {
+      setPostalCode({ ...city, error: postalCodeError });
+    }
+    if (addressError) {
+      setAddress({ ...city, error: addressError });
+    }
+    if (
+      !addressError &&
+      !postalCodeError &&
+      !cityError &&
+      !priceError &&
+      !descriptionError &&
+      !titleError
+    ) {
+      console.log(
+        `send data to apollo mutation hook: `,
+        title.value,
+        description.value,
+        price.value,
+        city.value,
+        postalCode.value,
+        address.value
+      );
+    }
+  };
+
+  const showAlert = () => {
+    Alert.alert(
+      'Please Allow Access',
+      [
+        'This applicaton needs access to your photo library to upload images.',
+        '\n\n',
+        'Please go to Settings of your device and grant permissions to Photos.',
+      ].join(''),
+      [
+        { text: 'Not Now', style: 'cancel' },
+        { text: 'Settings', onPress: () => Linking.openURL('app-settings:') },
+      ]
+    );
+  };
+
+  const getImagePermission = async () => {
+    //Only neccessary for ios
+    if (Constants.platform.ios) {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== 'granted') {
+        return showAlert();
+      }
+    }
+    return pickImage();
+  };
+
+  const pickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    if (!result.cancelled) {
+      setImage({ ...images, uri: [...images.uri, result.uri] });
+    }
+  };
+
+  const removePicture = imageURI => {
+    const newImages = images.uri.filter(uri => uri !== imageURI);
+    setImage({ uri: newImages, error: '' });
+  };
+
   return (
     <Background>
       <Modal visible={modalOpen}>
-        <MaterialIcons
-          name="close"
-          size={24}
-          style={{ ...styles.modalToggle, ...styles.modalClose }}
-          onPress={() => setModalOpen(false)}
-        />
-        <Background>
-          <Text>Hello modal</Text>
-          <Button
-            mode="contained"
-            onPress={() => console.log('post job via apollo hook')}
-          >
-            POST JOB
-          </Button>
-        </Background>
+        <ScrollView>
+          <Container>
+            <MaterialIcons
+              name="close"
+              size={24}
+              style={{ ...styles.modalToggle, ...styles.modalClose }}
+              onPress={() => setModalOpen(false)}
+            />
+            <TextInput
+              label="Title"
+              returnKeyType="next"
+              value={title.value}
+              onChangeText={text => setTitle({ value: text, error: '' })}
+              error={!!title.error}
+              errorText={title.error}
+            />
+            <TextInput
+              style={styles.desription}
+              label="Description"
+              returnKeyType="next"
+              value={description.value}
+              onChangeText={text => setDescription({ value: text, error: '' })}
+              error={!!description.error}
+              errorText={description.error}
+              multiline={true}
+            />
+            <TextInput
+              //TODO: Render currency code for the country the user belongs to
+              label="Price"
+              returnKeyType="next"
+              value={price.value}
+              onChangeText={text => setPrice({ value: text, error: '' })}
+              error={!!price.error}
+              errorText={price.error}
+              keyboardType={'numeric'}
+            />
+            <Button icon="camera" mode="contained" onPress={getImagePermission}>
+              ADD IMAGE
+            </Button>
+            <View style={styles.imageContainer}>
+              {images.uri.length >= 1 &&
+                images.uri.map(imageURI => {
+                  return (
+                    <TouchableHighlight
+                      key={imageURI}
+                      onPress={() => removePicture(imageURI)}
+                    >
+                      <Image
+                        style={styles.image}
+                        source={{ uri: imageURI }}
+                      ></Image>
+                    </TouchableHighlight>
+                  );
+                })}
+            </View>
+            {images.uri.length >= 1 && (
+              <Text style={styles.text}>Press an image to remove it </Text>
+            )}
+            <TextInput
+              //TODO: Render list of predefined cities based on country user belongs to
+              label="City"
+              returnKeyType="next"
+              value={city.value}
+              onChangeText={text => setCity({ value: text, error: '' })}
+              error={!!city.error}
+              errorText={city.error}
+            />
+            <TextInput
+              label="PostalCode"
+              returnKeyType="next"
+              value={postalCode.value}
+              onChangeText={text => setPostalCode({ value: text, error: '' })}
+              error={!!postalCode.error}
+              errorText={postalCode.error}
+            />
+            <TextInput
+              label="Address"
+              returnKeyType="next"
+              value={address.value}
+              onChangeText={text => setAddress({ value: text, error: '' })}
+              error={!!address.error}
+              errorText={address.error}
+            />
+            <Button mode="contained" onPress={validateInput}>
+              PUBLISH JOB
+            </Button>
+          </Container>
+        </ScrollView>
       </Modal>
       <Button icon="plus" mode="contained" onPress={() => setModalOpen(true)}>
         POST JOB
@@ -48,10 +245,27 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 10,
     alignSelf: 'center',
+    color: theme.colors.primary,
   },
   modalClose: {
     marginTop: 20,
     marginBottom: 0,
+  },
+  imageContainer: {
+    flexDirection: 'row',
+  },
+  image: {
+    marginRight: 5,
+    marginBottom: 5,
+    width: 50,
+    height: 50,
+  },
+  text: {
+    color: theme.colors.secondary,
+  },
+  desription: {
+    height: 100,
+    backgroundColor: '#ffffff',
   },
 });
 
